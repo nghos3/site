c72a86c7bf9c2696a756f2a2374819c6532aecca
@nirmalya.ghosh <nirmalya.ghosh@dish.com>
2024-10-08 14:41:46 +0530

    stylings

--- src/Components/Instruction.js
+++ src/Components/Instruction.js
@@ -3,7 +3,7 @@ import React from 'react';
     return (<div class="dc-doc-layout__center">
         <h1>Divkit</h1>
         <h4>Click on the side Navigation tabs to go through working of various Functionalities that divkit can do</h4>
-        <h6 style={{color: 'red'}}>{`(Note: Some examples are currently not working as they are hosted by the divkit website which is blocked by our organization we will try to bring a fix)`}</h6>
+        <h6 style={{color: 'red'}}>{`(Note: the interactive examples are currently not working as they are hosted by the divkit website which is blocked by our organization we will try to bring a fix)`}</h6>
     </div>)
  }
 export default Components;
